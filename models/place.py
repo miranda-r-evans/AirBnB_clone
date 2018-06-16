f1dfbf7043cb03b0807a8d83800deb789e71649e
mirandarevans <miranda.r.evans@gmail.com>
2018-06-15 18:39:03 -0700

    added new attribute to place

--- models/place.py
+++ models/place.py
@@ -7,19 +7,20 @@ from models.base_model import BaseModel
 class Place(BaseModel):
         ''' A place for rent, in a location and with serveral features '''
 
-        class_att_dict = {'city_id': str, 'state_id': str, 'name': str,
+        class_att_dict = {'city_id': str, 'user_id': str, 'name': str,
                           'description': str, 'number_rooms': int,
                           'number_bathrooms': int, 'max_guest': int,
-                          'price_by_night': float, 'latitude': float,
-                          'longitude': float}
+                          'price_by_night': int, 'latitude': float,
+                          'longitude': float, 'amenity_id': list}
 
         city_id = ''
-        state_id = ''
+        user_id = ''
         name = ''
         description = ''
         number_rooms = 0
         number_bathrooms = 0
         max_guest = 0
-        price_by_night = 0.0
+        price_by_night = 0
         latitude = 0.0
         longitude = 0.0
+        amenity_id = []
